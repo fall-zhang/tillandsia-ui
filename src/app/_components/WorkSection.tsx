9a3b2d28e0481daeee339160a59b6e5d014eb3e4
fall-zhang <zhangbofei@163.com>
2025-05-13 16:31:23 +0800

    更新 components 组件

--- src/app/_components/WorkSection.tsx
+++ src/app/_components/WorkSection.tsx
@@ -9,7 +9,7 @@ const WorkSection:FC<{
 }) => {
   return <a
     href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+    className="group rounded-lg border border-transparent px-5 py-4 w-full  transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
     target="_blank"
     rel="noopener noreferrer"
   >
